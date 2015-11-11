baacd733928713bc9408551cb75a2ba551798cba
Tony Bowden <tony@tmtm.com>
2015-11-11 15:41:49 +0000

    ping rebuilder

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,7 @@
 
 require 'scraperwiki'
 require 'wikidata/fetcher'
+require 'rest-client'
 
 @pages = [ 'Categoría:Diputados_de_la_X_Legislatura_de_España' ]
 
@@ -10,3 +11,6 @@ require 'wikidata/fetcher'
   data = WikiData::Fetcher.new(id: id).data('es') or next
   ScraperWiki.save_sqlite([:id], data)
 end
+
+warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+
