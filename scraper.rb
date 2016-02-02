bac52a7c976567a73502c0322a50e661ecf7d725
Tony Bowden <tony@tmtm.com>
2016-02-02 09:08:03 +0000

    Term 11

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,9 @@
 
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new('Categoría:Diputados_de_la_X_Legislatura_de_España', 'es').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { es: names }, output: false)
+names = {
+  10 => WikiData::Category.new('Categoría:Diputados_de_la_X_Legislatura_de_España', 'es').member_titles,
+  11 => WikiData::Category.new('Categoría:Diputados_de_la_XI_Legislatura_de_España', 'es').member_titles,
+}
+EveryPolitician::Wikidata.scrape_wikidata(names: { es: names.values.flatten.uniq }, output: false)
 warn EveryPolitician::Wikidata.notify_rebuilder
