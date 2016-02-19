ae9d04270b96fb9959c779890d9e8e0fe3ed7c0f
Tony Bowden <tony@tmtm.com>
2016-02-19 11:57:49 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -7,5 +7,9 @@ names = {
   10 => WikiData::Category.new('Categoría:Diputados_de_la_X_Legislatura_de_España', 'es').member_titles,
   11 => WikiData::Category.new('Categoría:Diputados_de_la_XI_Legislatura_de_España', 'es').member_titles,
 }
-EveryPolitician::Wikidata.scrape_wikidata(names: { es: names.values.flatten.uniq }, output: false)
+
+ids = EveryPolitician::Wikidata.wdq('claim[463:21857364]')
+binding.pry
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { es: names.values.flatten.uniq }, output: false)
 warn EveryPolitician::Wikidata.notify_rebuilder
